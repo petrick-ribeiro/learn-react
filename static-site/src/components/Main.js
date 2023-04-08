396648200dcfa790dd89c823922a37d6b849bddb
petrick-ribeiro <ptrck1110@protonmail.com>
2023-04-08 04:19:37 -0300

    First Commit

---
+++ static-site/src/components/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="main">
+      <h1 className="main-title">FUN FACTS ABOUT REACT</h1>
+      <ul className="main-facts">
+        <li>Was first released in 2013.</li>
+        <li>Was originally create.</li>
+        <li>Has well over 100K stars on GitHub.</li>
+        <li>Is maintained by Meta.</li>
+        <li>Powers thousands of enterprise apps, including mobile apps.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Main;
---
+++ static-site/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="nav">
+      <img
+        className="nav-logo"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+      />
+      <h3 className="nav-logo-text">ReactFacts</h3>
+      <h4 className="nav-title">React Static Example</h4>
+    </nav>
+  );
+}
+
+export default Navbar;
