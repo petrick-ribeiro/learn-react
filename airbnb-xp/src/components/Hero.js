9cb0f17d80079fd5cf2f253d8ead27b021729385
petrick-ribeiro <ptrck1110@protonmail.com>
2023-04-11 01:48:35 -0300

    Fix Hero component text

--- airbnb-xp/src/components/Hero.js
+++ airbnb-xp/src/components/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
       <img className="hero-grid" src="/images/grid.png" />
       <h1 className="hero-header">Online Experiences</h1>
       <p className="hero-text">
-        Join unique interactive activities les by one-of-a-king hosts-all
+        Join unique interactive activities led by one-of-a-king hosts-all
         without leaving home.
       </p>
     </section>
